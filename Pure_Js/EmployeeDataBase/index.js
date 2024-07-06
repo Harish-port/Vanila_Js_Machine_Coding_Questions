e6211753d71319adcf3c82fdb16d291dfeb5fa8b
Harish S <harish.sq@gmail.com>
2024-07-06 23:21:39 +0530

    changes of pure js

---
+++ Pure_Js/EmployeeDataBase/index.js
@@ -0,0 +1,8 @@
+(async function () {
+  const data = await fetch("./data.json");
+  const res = await data.json();
+  let employees = res;
+  let selectedEmployee = employees[0];
+  let selectedEmployeeId = employees[0].id;
+  const emmplyo
+})();
