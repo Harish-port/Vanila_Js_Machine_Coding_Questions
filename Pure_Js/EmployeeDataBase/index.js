f950c81350537784951f0e1dc896afd165de7233
Harish S <harish.sq@gmail.com>
2024-07-28 22:24:48 +0530

    added

--- Pure_Js/EmployeeDataBase/index.js
+++ Pure_Js/EmployeeDataBase/index.js
@@ -1,40 +1,78 @@
 (async function () {
   const data = await fetch("./data.json");
   const res = await data.json();
-  console.log(res, "response");
+
   let employees = res;
-  let selectedEmployee = employees[0];
   let selectedEmployeeId = employees[0].id;
+  let selectedEmployee = employees[0];
+
   const employeeList = document.querySelector(".employees__names--list");
   const employeeInfo = document.querySelector(".employees__single--info");
-  // Add Employee Logic
+
+  // Add Employee - START
   const createEmployee = document.querySelector(".createEmployee");
   const addEmployeeModal = document.querySelector(".addEmployee");
-  const addEmployeeForm = document.querySelector(".addEmployee");
+  const addEmployeeForm = document.querySelector(".addEmployee_create");
+
   createEmployee.addEventListener("click", () => {
     addEmployeeModal.style.display = "flex";
   });
-  addEmployeeModal.addEventListener("click", () => {
+
+  addEmployeeModal.addEventListener("click", (e) => {
     if (e.target.className === "addEmployee") {
       addEmployeeModal.style.display = "none";
     }
   });
+
+  // Set Employee age to be entered minimum 18 years
+  const dobInput = document.querySelector(".addEmployee_create--dob");
+  dobInput.max = `${new Date().getFullYear() - 18}-${new Date()
+    .toISOString()
+    .slice(5, 10)}`;
+
   addEmployeeForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(addEmployeeForm);
-    const values = [formData.entries()];
+    const values = [...formData.entries()];
     let empData = {};
     values.forEach((val) => {
-      empData[val[0]];
+      empData[val[0]] = val[1];
     });
+    empData.id = employees[employees.length - 1].id + 1;
+    empData.age =
+      new Date().getFullYear() - parseInt(empData.dob.slice(0, 4), 10);
+    empData.imageUrl =
+      empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+    employees.push(empData);
+    renderEmployees();
+    addEmployeeForm.reset();
+    addEmployeeModal.style.display = "none";
   });
-  // Select Employee Logic
+  // Add Employee - END
+//Edit Employee
+
   employeeList.addEventListener("click", (e) => {
+    // Select Employee Logic - START
     if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
       selectedEmployeeId = e.target.id;
       renderEmployees();
       renderSingleEmployee();
     }
+    // Select Employee Logic - END
+
+    // Employee Delete Logic - START
+    if (e.target.tagName === "I") {
+      employees = employees.filter(
+        (emp) => String(emp.id) !== e.target.parentNode.id
+      );
+      if (String(selectedEmployeeId) === e.target.parentNode.id) {
+        selectedEmployeeId = employees[0]?.id || -1;
+        selectedEmployee = employees[0] || {};
+        renderSingleEmployee();
+      }
+      renderEmployees();
+    }
+    // Employee Delete Logic - END
   });
 
   // Render All Employees Logic - START
@@ -52,17 +90,30 @@
       employeeList.append(employee);
     });
   };
+  // Render All Employees Logic - END
 
+  // Render Single Employee Logic - START
   const renderSingleEmployee = () => {
+    // Employee Delete Logic - START
+    if (selectedEmployeeId === -1) {
+      employeeInfo.innerHTML = "";
+      return;
+    }
+    // Employee Delete Logic - END
+
     employeeInfo.innerHTML = `
-    <img src="${selectedEmployee.imageUrl}"/>
-    <span class='employees__single--info'>${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})</span>
-    <span>${selectedEmployee.address}</span>
-    <span>${selectedEmployee.email}</span>
-    <span>Mobile - ${selectedEmployee.contactNumber}</span>
-    <span>DOB - ${selectedEmployee.dob}</span>
+      <img src="${selectedEmployee.imageUrl}" />
+      <span class="employees__single--heading">
+      ${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})
+      </span>
+      <span>${selectedEmployee.address}</span>
+      <span>${selectedEmployee.email}</span>
+      <span>Mobile - ${selectedEmployee.contactNumber}</span>
+      <span>DOB - ${selectedEmployee.dob}</span>
     `;
   };
-  if (selectedEmployee) renderSingleEmployee();
+  // Render Single Employee Logic - END
+
   renderEmployees();
+  if (selectedEmployee) renderSingleEmployee();
 })();
